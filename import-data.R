32f452590c9494ec440120d93c19fd66200f16df
Aaron Kessler <kessler.aaron@gmail.com>
2023-11-28 16:00:19 -0500

    transition to class google form survey

--- import-data.R
+++ import-data.R
@@ -1,4 +1,4 @@
-# This sequence is from R for the Rest of Us, located here:
+# This sequence based on R for the Rest of Us:
 # https://rfortherestofus.com/2023/05/github-actions
 
 
@@ -8,10 +8,7 @@ library(googlesheets4)
 # The data is publicly available so we don't need to authenticate
 gs4_deauth()
 
-survey_data <- read_sheet("https://docs.google.com/spreadsheets/d/13kfPtyQP1xmL4Rn6rfJHgJcAblfH7pxS5RvdmGe6BHg/edit?usp=sharing")
-
-survey_data %>%
-  write_rds("survey_data.rds")
+survey_data <- read_sheet("https://docs.google.com/spreadsheets/d/16o68CSXpNWzVVk-rvfjQF20He_2UqopOcxshFo74S4U/edit?usp=sharing")
 
 survey_data %>%
   write_csv("survey_data.csv")
